1474b2c64d3fa0e4fb9453e763e311698894f36e
bnaya eshet <bnayanet@hotmail.com>
2020-03-22 20:01:40 +0200

    typescript & styled component

---
+++ pages/_document.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Document, { Head, NextScript, Main } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <Head />
+        <link rel="stylesheet" href="/Gilroy/stylesheet.css" />
+        <Main />
+        <NextScript />
+      </>
+    );
+  }
+}
+
+export default MyDocument;
